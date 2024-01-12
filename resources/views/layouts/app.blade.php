15e5f75f621796965f5f4ddd4ba8f78892d623fb
Jeroen922 <82508001+Jeroen922@users.noreply.github.com>
2024-01-12 10:00:37 +0100

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,7 +32,7 @@
 <div id="app">
     <nav class="navbar navbar-expand-md navbar-light navbar-bg shadow-sm">
         <div class="container">
-            <a class="navbar-brand" href="{{ url('/') }}">
+            <a class="navbar-brand" href="{{ url('/home') }}">
                 <img src="images\Logo-f1.jpg" class="" />
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
