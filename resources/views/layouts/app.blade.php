7aa0f7ebf0c7685fc99067136bf008d73a6aba1e
Jeroen922 <82508001+Jeroen922@users.noreply.github.com>
2024-01-12 10:07:07 +0100

    logo op altpagina's

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,7 +33,7 @@
     <nav class="navbar navbar-expand-md navbar-light navbar-bg shadow-sm">
         <div class="container">
             <a class="navbar-brand" href="{{ url('/home') }}">
-                <img src="images\Logo-f1.jpg" class="" />
+                <img src="\images\Logo-f1.jpg" class="" />
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                     data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
