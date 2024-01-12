aaa2443577945f435d300ba14893bab04e24516d
AshrafBasnoe-Github <s1185527@student.windesheim.nl>
2024-01-12 09:44:39 +0100

    pushu
    pushu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -58,7 +58,7 @@
                                     </a>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="dropdown-item" href="{{route('profiles.createProfiles')}}">
+                                    <a class="dropdown-item" href="{{route('profiles.create')}}">
                                          Toevoegen
                                     </a>
                                 </li>
@@ -91,7 +91,7 @@
                         <div class="dropdown-menu dropdown-menu-begin" aria-labelledby="navbarDropdownScoreboard">
                             <ul class="navbar-nav me-auto">
                                 <li class="nav-item">
-                                    <a class="dropdown-item" href=" {{route('')}}">
+                                    <a class="dropdown-item" href="">
                                         Bekijken
                                     </a>
                                 </li>
