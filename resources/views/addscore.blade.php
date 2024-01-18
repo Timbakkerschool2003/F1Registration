98b5cf1dec641989f5e13acab8618434fd9c40d6
Tim Bakker <s1185808@student.windesheim.nl>
2024-01-18 22:57:33 +0100

    Styling toegevoegd
    ff snel styling

--- resources/views/addscore.blade.php
+++ resources/views/addscore.blade.php
@@ -1,44 +1,56 @@
 @extends('layouts.app')
 
 @section('content')
-    <h2>Add Score</h2>
+    <div class="container">
+        <h2>Add Score</h2>
 
-    @if(session('success'))
-        <div class="alert alert-success">
-            {{ session('success') }}
-        </div>
-    @endif
+        @if(session('success'))
+            <div class="alert alert-success">
+                {{ session('success') }}
+            </div>
+        @endif
 
-    <form method="POST" action="{{ route('addscore.store') }}">
-        @csrf
+        <form method="POST" action="{{ route('addscore.store') }}">
+            @csrf
 
-        <label for="user_id">User:</label>
-        <select name="users_id" id="user_id">
-            @foreach($users as $user)
-                <option value="{{ $user->id }}">{{ $user->name }}</option>
-            @endforeach
-        </select>
+            <div class="form-group">
+                <label for="user_id">User:</label>
+                <select name="users_id" id="user_id" class="form-control">
+                    @foreach($users as $user)
+                        <option value="{{ $user->id }}">{{ $user->name }}</option>
+                    @endforeach
+                </select>
+            </div>
 
-        <label for="time">Time:</label>
-        <input type="text" name="time" id="time">
+            <div class="form-group">
+                <label for="time">Time:</label>
+                <input type="text" name="time" id="time" class="form-control">
+            </div>
 
-        <label for="teams_id">Team:</label>
-        <select name="teams_id" id="teams_id">
-            @foreach($teams as $team)
-                <option value="{{ $team->id }}">{{ $team->name }}</option>
-            @endforeach
-        </select>
+            <div class="form-group">
+                <label for="teams_id">Team:</label>
+                <select name="teams_id" id="teams_id" class="form-control">
+                    @foreach($teams as $team)
+                        <option value="{{ $team->id }}">{{ $team->name }}</option>
+                    @endforeach
+                </select>
+            </div>
 
-        <label for="circuits_id">Circuit:</label>
-        <select name="circuits_id" id="circuit_id">
-            @foreach($circuits as $circuit)
-                <option value="{{ $circuit->id }}">{{ $circuit->name }}</option>
-            @endforeach
-        </select>
+            <div class="form-group">
+                <label for="circuits_id">Circuit:</label>
+                <select name="circuits_id" id="circuit_id" class="form-control">
+                    @foreach($circuits as $circuit)
+                        <option value="{{ $circuit->id }}">{{ $circuit->name }}</option>
+                    @endforeach
+                </select>
+            </div>
 
-        <label for="date">Date:</label>
-        <input type="date" name="date" id="date">
+            <div class="form-group">
+                <label for="date">Date:</label>
+                <input type="date" name="date" id="date" class="form-control">
+            </div>
 
-        <button type="submit">Submit</button>
-    </form>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
 @endsection
